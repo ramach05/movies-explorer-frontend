636458253c626ca1ab88d54580850b3876e6d443
Roman <mrroman6552@gmail.com>
2021-06-11 06:04:24 +0300

    feat: markup Techs

--- src/components/Techs/Techs.js
+++ src/components/Techs/Techs.js
@@ -1,8 +1,24 @@
+import "./Techs.css";
+
 function Techs() {
   return (
-    <>
-      <div>Techs</div>
-    </>
+    <section className="techs">
+      <h2 className="techs__title">Технологии</h2>
+      <h3 className="techs__subtitle">7 технологий</h3>
+      <p className="techs__text">
+        На курсе веб-разработки мы освоили технологии, которые применили в
+        дипломном проекте.
+      </p>
+      <div className="techs__grid-container">
+        <p className="techs__grid-container__item">HTML</p>
+        <p className="techs__grid-container__item">CSS</p>
+        <p className="techs__grid-container__item">JS</p>
+        <p className="techs__grid-container__item">React</p>
+        <p className="techs__grid-container__item">Git</p>
+        <p className="techs__grid-container__item">Express.js</p>
+        <p className="techs__grid-container__item">mongoDB</p>
+      </div>
+    </section>
   );
 }
 
