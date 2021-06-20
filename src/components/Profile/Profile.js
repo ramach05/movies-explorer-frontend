2463ef5194748d7ae656b9803287f56967c7d2ae
Roman <mrroman6552@gmail.com>
2021-06-20 08:23:55 +0300

    refactor: Profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -12,18 +12,19 @@ function Profile() {
           <li className="profile__li">
             <input
               type="text"
-              id="person-name"
-              name="person-name"
+              id="profile-input-name"
               className="profile__input"
             />
-            <label htmlFor="person-name" className="profile__input__label">Имя</label>
+            <label htmlFor="profile-input-name" className="profile__input__label">Имя</label>
           </li>
           <li className="profile__li">
             <input
               type="email"
+              value="pochta@yandex.ru"
+              id="profile-input-email"
               className="profile__input"
             />
-            <label htmlFor="person-name" className="profile__input__label">E-mail</label>
+            <label htmlFor="profile-input-email" className="profile__input__label">E-mail</label>
           </li>
         </ul>
         <button type="submit" className="profile__form-submit">Редактировать</button>
